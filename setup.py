8d4c6a133115ff0be2a5cfed9c997321207fa85d
Vopaaz <liyifan945@163.com>
2019-01-16 16:33:09 +0800

    Package and distribute

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(r'README.md',"r",encoding="utf8") as f:
 
 setup(
     name='cnki2bib',
-    version='0.1.0',
+    version='0.1.2',
     author='Vopaaz',
     author_email="liyifan945@163.com",
     url="https://github.com/Vopaaz/CNKI_2_BibTeX",
