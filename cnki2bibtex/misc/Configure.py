a108aeab17493cdb5e664a59035b60e28b463586
Vopaaz <liyifan945@163.com>
2019-03-14 20:00:23 +0800

    Fix: change wrong option encoding="utf8" to "utf-8"
    Also adjust the format. and update version in setup.py

--- cnki2bibtex/misc/Configure.py
+++ cnki2bibtex/misc/Configure.py
@@ -1,16 +1,18 @@
 import os
 import re
 
+
 def setIDFormat(idFormat):
-    filePath = os.path.join(os.path.expanduser('~'),r".cnki2bib.cfg")
-    with open(filePath, "w",encoding="utf8") as f:
+    filePath = os.path.join(os.path.expanduser('~'), r".cnki2bib.cfg")
+    with open(filePath, "w", encoding="utf-8") as f:
         f.write("[settings]\nid_format = {}".format(idFormat))
 
+
 def getIDFormat():
-    filePath = os.path.join(os.path.expanduser('~'),r".cnki2bib.cfg")
+    filePath = os.path.join(os.path.expanduser('~'), r".cnki2bib.cfg")
     if os.path.exists(filePath):
-        with open(filePath, "r",encoding="utf8") as f:
+        with open(filePath, "r", encoding="utf-8") as f:
             configStr = f.read()
-        return re.search(r"id_format = (.*)",configStr).group(1)
+        return re.search(r"id_format = (.*)", configStr).group(1)
     else:
         return "title"
