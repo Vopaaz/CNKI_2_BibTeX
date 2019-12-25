94a15e1ea10a313bc705be59e7648ce51a72ef78
Vopaaz <liyifan945@163.com>
2019-12-25 08:40:57 -0800

    Fix failed tests caused by id_format

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import glob
 from cnki2bibtex.cnki2bib import getBibFileContentString
+from cnki2bibtex.misc.Configure import setIDFormat
 import os
 import unittest
 import sys
@@ -7,12 +8,28 @@ sys.path.append(".")
 
 
 class IntegrationTest(unittest.TestCase):
-    def test_all(self):
+    all_net = glob.glob("./tests/assets/*.net")
+
+    def _test_bib_net_pair(self, bib, net):
         self.maxDiff = 1000000
-        all_bib = glob.glob("./tests/assets/*.bib")
-        for bib in all_bib:
-            net = os.path.splitext(bib)[0] + ".net"
-            with open(bib, "r", encoding="utf-8") as b,\
-                    open(net, "r", encoding="utf-8") as n:
-                self.assertEqual(b.read().replace("\n", "").replace("\r", ""),
-                                 getBibFileContentString(n.read()).replace("\n", "").replace("\r", ""))
+        with open(bib, "r", encoding="utf-8") as b,\
+                open(net, "r", encoding="utf-8") as n:
+            self.assertEqual(b.read(),
+                             getBibFileContentString(n.read()))
+
+    def _test_format(self, format_):
+        setIDFormat(format_)
+        for net in self.all_net:
+            dir_, file_ = os.path.split(net)
+            bib_dir = os.path.join(dir_, format_)
+            bib_file = os.path.splitext(file_)[0] + ".bib"
+            self._test_bib_net_pair(
+                os.path.join(bib_dir, bib_file),
+                net
+            )
+
+    def test_nameyear(self):
+        self._test_format("nameyear")
+
+    def test_title(self):
+        self._test_format("title")
